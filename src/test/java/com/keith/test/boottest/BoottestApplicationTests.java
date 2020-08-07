1a160466927c624b3f33496a56562d8d6f543465
keith <keith@staritgp.com>
2020-08-08 02:00:45 +0800

    1

--- src/test/java/com/keith/test/boottest/BoottestApplicationTests.java
+++ src/test/java/com/keith/test/boottest/BoottestApplicationTests.java
@@ -1,13 +1,27 @@
 package com.keith.test.boottest;
 
+import com.keith.test.boottest.entity.LogisticsService;
+import com.keith.test.boottest.mapper.LogisticsServiceMapper;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @SpringBootTest
-class BoottestApplicationTests {
+@RunWith(SpringRunner.class)
+public class BoottestApplicationTests {
+    @Autowired
+    LogisticsServiceMapper logisticsServiceMapper;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        List<LogisticsService> logisticsServices = logisticsServiceMapper.listAllLogisticsService();
+        for (LogisticsService logisticsService : logisticsServices) {
+            System.out.println(logisticsService);
+        }
     }
 
 }
